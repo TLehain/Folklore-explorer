fe72d1856a4e98f0f054cd95f56cae6bfe2cac16
TLehain <46997082+TLehain@users.noreply.github.com>
2025-06-09 12:48:46 +0100

    Update walks.js

--- js/walks.js
+++ js/walks.js
@@ -360,7 +360,7 @@ class WalksManager {
     }
     return null;
     }
-// Global success message function
+  
 window.showSuccess = function(message) {
   const successDiv = document.createElement('div');
   successDiv.className = 'success-notification';
